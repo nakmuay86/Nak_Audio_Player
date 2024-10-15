e937ebbc83973273258f2159c0a261a2ac61465b
Gadzhi <88350812+nakmuay86@users.noreply.github.com>
2024-10-15 20:59:29 +0300

    Nak Audio Player v2
    + 5 audio files
    + new design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React from 'react';
 import './App.css';
 
-import AudioFile from './Components/audio.mp3'
+import AudioFile1 from './Components/deadmau5 — The Oshawa Connection.mp3';
+import AudioFile2 from './Components/deadmau5 — Unspecial Effects.mp3';
+import AudioFile3 from './Components/deadmau5 — Messages from nowhere.mp3';
+import AudioFile4 from './Components/deadmau5 — Sometimes I Fail.mp3';
+import AudioFile5 from './Components/deadmau5 — Waking Up From the American Dream.mp3';
+import AudioFile6 from './Components/deadmau5 — FlashTV.mp3';
+
 import AudioPlayer from './Components/AudioPlayer';
+
+// Список аудиофайлов
+const audioFiles = [
+  AudioFile1,
+  AudioFile2,
+  AudioFile3,
+  AudioFile4,
+  AudioFile5,
+  AudioFile6,
+];
 
 function App() {
   return (
     <div>
-      <AudioPlayer audioFile={AudioFile}/>
+      <AudioPlayer audioFiles={audioFiles} />
     </div>
   );
 }
--- src/Components/AudioPlayer.js
+++ src/Components/AudioPlayer.js
@@ -1,154 +1,153 @@
-import {useRef, useEffect, useState} from 'react';
+import React, { useRef, useEffect, useState, useCallback } from 'react';
 import WaveSurfer from 'wavesurfer.js';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {
-   faPlay,
-   faPause,
-   faVolumeUp,
-   faVolumeDown,
-   faVolumeMute,
-   faVolumeOff,
-} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPause, faVolumeUp, faVolumeDown, faVolumeMute, faVolumeOff } from '@fortawesome/free-solid-svg-icons';
 
-
+// Функция для конфигурации WaveSurfer
 const formWaveSurferOptions = (ref) => ({
-    container: ref,
-    waveColor: '#ccc',
-    progressColor: 'orange',
-    cursorColor: 'transparent',
-    responsive: true,
-    height: 80,
-    normalize: true,
-    backend: 'WebAudio',
-    barWidth: 2,
-    barGap: 3,
-})
+  container: ref,
+  waveColor: '#ccc',
+  progressColor: 'red',
+  cursorColor: 'transparent',
+  responsive: true,
+  height: 55,
+  normalize: true,
+  backend: 'WebAudio',
+  barWidth: 2,
+  barGap: 3,
+});
 
-// Helper function to format time
-function formatTime(seconds) {
-    let date = new Date (0);
-    date.setSeconds(seconds);
-    return date.toISOString().substr(11, 8)
-}
+// Функция для форматирования времени
+const formatTime = (seconds) => {
+  const date = new Date(0);
+  date.setSeconds(seconds);
+  return date.toISOString().substr(11, 8);
+};
 
-export default function AudioPlayer({audioFile}) {
-    const waveformRef = useRef(null);
-    const wavesurfer = useRef(null);
-    const [playing, setPlaying] = useState(false);
-    const [volume, setVolume] = useState(0.5);
-    const [muted, setMuted] = useState(false);
-    const [duration, setDuration] = useState(0);
-    const [currentTime, setCurrentTime] = useState(0);
-    const [audioFileName, setAudioFileName] = useState('');
+// Компонент аудиоплеера
+export default function AudioPlayer({ audioFiles }) {
+  const waveformRef = useRef(null);
+  const wavesurfer = useRef(null);
+  const [playing, setPlaying] = useState(false);
+  const [volume, setVolume] = useState(0.5);
+  const [muted, setMuted] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [audioFileIndex, setAudioFileIndex] = useState(0);
+  const [audioFileName, setAudioFileName] = useState('');
 
-// Initialize WaveSurfer and set up event listeners
-  useEffect(()=>{
-    // Create WaveSurfer instance with options
+  // Переход к следующему аудиофайлу
+  const handleNextAudio = useCallback(() => {
+    const nextIndex = (audioFileIndex + 1) % audioFiles.length;
+    setAudioFileIndex(nextIndex);
+    setCurrentTime(0);
+    wavesurfer.current.stop();
+    setPlaying(false);
+  }, [audioFileIndex, audioFiles]);
+
+  // Инициализация WaveSurfer
+  useEffect(() => {
+    if (audioFiles.length === 0) return;
+
     const options = formWaveSurferOptions(waveformRef.current);
     wavesurfer.current = WaveSurfer.create(options);
 
-// Load the Audio File
-wavesurfer.current.load(audioFile);
+    const loadAudioFile = () => {
+      wavesurfer.current.load(audioFiles[audioFileIndex]);
+      setPlaying(false);
+    };
 
-// When WaveSurfer is ready
-wavesurfer.current.on('ready', () => {
-    setVolume(wavesurfer.current.getVolume());
-    setDuration(wavesurfer.current.getDuration());
-    setAudioFileName(audioFile.split('/').pop());
-});
+    loadAudioFile();
 
-// Update current time in state as audio plays
-wavesurfer.current.on('audioprocess', () => {
-    setCurrentTime(wavesurfer.current.getCurrentTime());
-});
+    wavesurfer.current.on('ready', () => {
+      setVolume(wavesurfer.current.getVolume());
+      setDuration(wavesurfer.current.getDuration());
+      setAudioFileName(audioFiles[audioFileIndex].split('/').pop());
+    });
 
-// Clean up event listeners and destroy instance on unmount
-return() => {
-    wavesurfer.current.un('audioprocess');
-    wavesurfer.current.un('ready');
-    wavesurfer.current.destroy();
-}
+    wavesurfer.current.on('audioprocess', () => {
+      setCurrentTime(wavesurfer.current.getCurrentTime());
+    });
 
-}, [audioFile]);
+    wavesurfer.current.on('finish', handleNextAudio);
 
-// Toggle playback of audio
-const handlePlayPause = () => {
-   setPlaying(!playing);
-   wavesurfer.current.playPause();
-};
+    return () => {
+      wavesurfer.current.un('audioprocess');
+      wavesurfer.current.un('ready');
+      wavesurfer.current.un('finish');
+      wavesurfer.current.destroy();
+    };
+  }, [audioFileIndex, audioFiles, handleNextAudio]);
 
-// Adjust audio volume
-const handleVolumeChange = (newVolume) => {
+  const handlePlayPause = () => {
+    setPlaying(!playing);
+    wavesurfer.current.playPause();
+  };
+
+  const handleVolumeChange = (newVolume) => {
     setVolume(newVolume);
     wavesurfer.current.setVolume(newVolume);
     setMuted(newVolume === 0);
-};
+  };
 
-// Toggle mute/unmute audio
-const handleMute = () => {
+  const handleMute = () => {
     setMuted(!muted);
-    wavesurfer.current.setVolume(muted ? volume: 0);
-};
+    wavesurfer.current.setVolume(muted ? volume : 0);
+  };
 
-// Increase volume by 10%
-const handleVolumeUp = () => {
+  const handleVolumeUp = () => {
     handleVolumeChange(Math.min(volume + 0.1, 1));
-};
+  };
 
-// Decrease volume by 10%
-const handleVolumeDown = () => {
+  const handleVolumeDown = () => {
     handleVolumeChange(Math.max(volume - 0.1, 0));
-};
-
+  };
 
   return (
     <div>
-    <img src="https://images.genius.com/4154e76a8d2d2a8ece304bf401a86c03.320x320x1.jpg" alt="Обложка" style={{ width: '100%', maxWidth: '400px' }} />        
-    <h2>Music Title: Extemporized ophthalmic - Melechesh</h2>
-    <div id='waveform' ref={waveformRef} style={{width: '100%'}}></div>
-        <div className="controls">
-
-           {/* Play/Pause button */} 
-            <button   onClick={handlePlayPause}> 
-                <FontAwesomeIcon  icon={playing ? faPause : faPlay}/>
-            </button>
-        
-        {/* Mute/Unmute button */} 
+      <img
+      src="https://e.snmc.io/i/1200/s/14d5fc8f0a88be404bfa1b89ef2d844b/10186837"
+      alt="Обложка"
+      class="responsive-image"
+       />
+      <h2>Music title: {audioFileName}</h2>
+      <div id="waveform" ref={waveformRef} style={{ width: '100%'}}></div>
+      <div className="controls">
+        <button onClick={handlePlayPause}>
+          <FontAwesomeIcon icon={playing ? faPause : faPlay} />
+        </button>
         <button onClick={handleMute}>
-                <FontAwesomeIcon icon={muted ? faVolumeOff : faVolumeMute}/>
-            </button>
-        
-        {/* Volume slider */}
-<input type='range'
-       id='volume'
-       name='volume'
-       min='0'
-       max='1'
-       step='0.05'
-       value={muted ? 0 : volume}
-       onChange={(e) => handleVolumeChange(parseFloat(e.target.value))}
-/>
-       {/* Volume Down Button */}
-    <button onClick={handleVolumeDown}>
-        <FontAwesomeIcon icon={faVolumeDown}/>
-    </button>
-
-    {/* Volume UP Button */}
-    <button onClick={handleVolumeUp}>
-        <FontAwesomeIcon icon={faVolumeUp}/>
-    </button>
-        </div>
-        <div className='audio-info'>
-            {/* Audio file name and current play time */}
-            <span>
-                Playing:{audioFileName} <br/>
-            </span>
-            <span>
-                Duration: {formatTime(duration)} | Current Time: {' '}
-                {formatTime(currentTime)} <br/>
-            </span>
-            <span>Volume: {Math.round(volume * 100)}%</span>
-        </div>
+          <FontAwesomeIcon icon={muted ? faVolumeOff : faVolumeMute} />
+        </button>
+        <input
+          type="range"
+          id="volume"
+          name="volume"
+          min="0"
+          max="1"
+          step="0.05"
+          value={muted ? 0 : volume}
+          onChange={(e) => handleVolumeChange(parseFloat(e.target.value))}
+        />
+        <button onClick={handleVolumeDown}>
+          <FontAwesomeIcon icon={faVolumeDown} />
+        </button>
+        <button onClick={handleVolumeUp}>
+          <FontAwesomeIcon icon={faVolumeUp} />
+        </button>
+        {/* Кнопка для перехода к следующей музыке */}
+        <div style={{ marginTop: '20px' }}>
+        {/* Кнопка для перехода к следующей музыке с отступом сверху */}
+        <button onClick={handleNextAudio}>
+          Next Music
+        </button>
+      </div>
+      </div>
+      <div className="audio-info">
+        <span>Music title: {audioFileName} <br /></span>
+        <span>Duration: {formatTime(duration)} | Текущее время: {formatTime(currentTime)} <br /></span>
+        <span>Volume: {Math.round(volume * 100)}%</span>
+      </div>
     </div>
   );
 }
